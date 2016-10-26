925b2a9d72c94f874361bf86ae62efde684c19f9
samlindstrom <sam.lindstrom@crownpeak.com>
2016-10-26 10:38:04 -0600

    Add record counter to report-header component

--- src/app/shared/report/report-header/report-header.component.ts
+++ src/app/shared/report/report-header/report-header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ReportHeaderComponent implements OnInit {
   @Input() title: string;
   @Input() summary: string;
+  @Input() records: any[];
 
 
   ngOnInit() {
